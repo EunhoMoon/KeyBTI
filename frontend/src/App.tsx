02db29d78eced6365b353730577215bca3d5be2e
Janek <eunho.moon@gmail.com>
2023-06-13 22:24:59 +0900

    Revert "init project"
    This reverts commit d5e149fb6ed423e6c13760206caa1b45415c0125.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,9 @@
-import React, {FC} from 'react';
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import RootLayout from "./page/RootLayout";
-import HomePage from "./page/HomePage";
-import TestPage from "./page/TestPage";
-import ResultPage from "./page/ResultPage";
+import React from 'react';
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout/>,
-        children: [
-            {index: true, element: <HomePage/>},
-            {path: 'test', element: <TestPage/>},
-            {path: 'result', element: <ResultPage/>}
-        ]
-    }
-]);
-
-const App:FC = () => {
-  return <RouterProvider router={router}/>;
+function App() {
+  return (
+      <div>new App!</div>
+  );
 }
 
 export default App;
